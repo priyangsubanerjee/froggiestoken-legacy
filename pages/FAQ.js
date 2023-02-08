bc3dc814706b01f36bb367d818e71026cf54c99a
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-02-08 23:08:24 +0530

    faq page

--- pages/FAQ.js
+++ pages/FAQ.js
@@ -1,18 +1,11 @@
 import NavbarDark from "@/components/NavbarDark";
 import React, { useState, useEffect } from "react";
 
-import { Disclosure } from "@headlessui/react";
+import Panel from "@/components/faq/panel";
 
 function FAQ() {
-  const [disclosureState, setDisclosureState] = useState(0);
+  const [state, setState] = useState(1);
 
-  function handleDisclosureChange(state) {
-    if (state === disclosureState) {
-      setDisclosureState(0); // close all of them
-    } else {
-      setDisclosureState(state); // open the clicked disclosure
-    }
-  }
   return (
     <div id="scrollContent" className="h-screen overflow-auto bg-primaryBg">
       <NavbarDark />
@@ -21,6 +14,86 @@ function FAQ() {
           <h1 className="text-4xl text-white font-bold uppercase font-poppins">
             Frequently asked questions
           </h1>
+        </div>
+        <div className="mt-10 px-32">
+          <Panel
+            index={1}
+            state={state}
+            setState={setState}
+            question={"How long has Froggies Token existed?"}
+            answer={
+              "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem eos perspiciatis aliquam nisi. Quas odio eos laudantium eveniet at soluta voluptatibus exercitationem tempora, accusamus harum minima voluptas in modi excepturi ratione aliquam recusandae distinctio cum a mollitia porro provident enim impedit non. Cum, hic modi. Praesentium necessitatibus consectetur saepe ducimus placeat veritatis perspiciatis modi dolores at eos aliquid quibusdam dignissimos iste autem magni omnis cupiditate repellat, laborum aut! Doloremque ipsum accusantium saepe doloribus a explicabo sapiente sint eaque maiores, unde nulla nihil quisquam harum quo dolorum perferendis vitae impedit sed. Ex ab voluptas, cumque voluptate sit aperiam debitis consequatur officia."
+            }
+          />
+          <Panel
+            index={2}
+            state={state}
+            setState={setState}
+            question={"What is our contract address?"}
+            answer={
+              "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem eos perspiciatis aliquam nisi. Quas odio eos laudantium eveniet at soluta voluptatibus exercitationem tempora, accusamus harum minima voluptas in modi excepturi ratione aliquam recusandae distinctio cum a mollitia porro provident enim impedit non. Cum, hic modi. Praesentium necessitatibus consectetur saepe ducimus placeat veritatis perspiciatis modi dolores at eos aliquid quibusdam dignissimos iste autem magni omnis cupiditate repellat, laborum aut! Doloremque ipsum accusantium saepe doloribus a explicabo sapiente sint eaque maiores, unde nulla nihil quisquam harum quo dolorum perferendis vitae impedit sed. Ex ab voluptas, cumque voluptate sit aperiam debitis consequatur officia."
+            }
+          />
+          <Panel
+            index={3}
+            state={state}
+            setState={setState}
+            question={"How secure is investing in Froggies Token?"}
+            answer={
+              "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem eos perspiciatis aliquam nisi. Quas odio eos laudantium eveniet at soluta voluptatibus exercitationem tempora, accusamus harum minima voluptas in modi excepturi ratione aliquam recusandae distinctio cum a mollitia porro provident enim impedit non. Cum, hic modi. Praesentium necessitatibus consectetur saepe ducimus placeat veritatis perspiciatis modi dolores at eos aliquid quibusdam dignissimos iste autem magni omnis cupiditate repellat, laborum aut! Doloremque ipsum accusantium saepe doloribus a explicabo sapiente sint eaque maiores, unde nulla nihil quisquam harum quo dolorum perferendis vitae impedit sed. Ex ab voluptas, cumque voluptate sit aperiam debitis consequatur officia."
+            }
+          />
+          <Panel
+            index={4}
+            state={state}
+            setState={setState}
+            question={"How secure is investing in Froggies Token?"}
+            answer={
+              "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem eos perspiciatis aliquam nisi. Quas odio eos laudantium eveniet at soluta voluptatibus exercitationem tempora, accusamus harum minima voluptas in modi excepturi ratione aliquam recusandae distinctio cum a mollitia porro provident enim impedit non. Cum, hic modi. Praesentium necessitatibus consectetur saepe ducimus placeat veritatis perspiciatis modi dolores at eos aliquid quibusdam dignissimos iste autem magni omnis cupiditate repellat, laborum aut! Doloremque ipsum accusantium saepe doloribus a explicabo sapiente sint eaque maiores, unde nulla nihil quisquam harum quo dolorum perferendis vitae impedit sed. Ex ab voluptas, cumque voluptate sit aperiam debitis consequatur officia."
+            }
+          />
+          <Panel
+            index={5}
+            state={state}
+            setState={setState}
+            question={"Is Froggies Token listed on a centralized exchange?"}
+            answer={
+              "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem eos perspiciatis aliquam nisi. Quas odio eos laudantium eveniet at soluta voluptatibus exercitationem tempora, accusamus harum minima voluptas in modi excepturi ratione aliquam recusandae distinctio cum a mollitia porro provident enim impedit non. Cum, hic modi. Praesentium necessitatibus consectetur saepe ducimus placeat veritatis perspiciatis modi dolores at eos aliquid quibusdam dignissimos iste autem magni omnis cupiditate repellat, laborum aut! Doloremque ipsum accusantium saepe doloribus a explicabo sapiente sint eaque maiores, unde nulla nihil quisquam harum quo dolorum perferendis vitae impedit sed. Ex ab voluptas, cumque voluptate sit aperiam debitis consequatur officia."
+            }
+          />
+          <Panel
+            index={6}
+            state={state}
+            setState={setState}
+            question={
+              "How to buy Froggies Token through the PancakeSwap website?"
+            }
+            answer={
+              "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem eos perspiciatis aliquam nisi. Quas odio eos laudantium eveniet at soluta voluptatibus exercitationem tempora, accusamus harum minima voluptas in modi excepturi ratione aliquam recusandae distinctio cum a mollitia porro provident enim impedit non. Cum, hic modi. Praesentium necessitatibus consectetur saepe ducimus placeat veritatis perspiciatis modi dolores at eos aliquid quibusdam dignissimos iste autem magni omnis cupiditate repellat, laborum aut! Doloremque ipsum accusantium saepe doloribus a explicabo sapiente sint eaque maiores, unde nulla nihil quisquam harum quo dolorum perferendis vitae impedit sed. Ex ab voluptas, cumque voluptate sit aperiam debitis consequatur officia."
+            }
+          />
+          <Panel
+            index={7}
+            state={state}
+            setState={setState}
+            question={
+              "How to fix the 'insufficient output' error on Pancakeswap?"
+            }
+            answer={
+              "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem eos perspiciatis aliquam nisi. Quas odio eos laudantium eveniet at soluta voluptatibus exercitationem tempora, accusamus harum minima voluptas in modi excepturi ratione aliquam recusandae distinctio cum a mollitia porro provident enim impedit non. Cum, hic modi. Praesentium necessitatibus consectetur saepe ducimus placeat veritatis perspiciatis modi dolores at eos aliquid quibusdam dignissimos iste autem magni omnis cupiditate repellat, laborum aut! Doloremque ipsum accusantium saepe doloribus a explicabo sapiente sint eaque maiores, unde nulla nihil quisquam harum quo dolorum perferendis vitae impedit sed. Ex ab voluptas, cumque voluptate sit aperiam debitis consequatur officia."
+            }
+          />
+          <Panel
+            index={8}
+            state={state}
+            setState={setState}
+            question={
+              "How can I become a liquidity provider and earn a piece of the pool?"
+            }
+            answer={
+              "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem eos perspiciatis aliquam nisi. Quas odio eos laudantium eveniet at soluta voluptatibus exercitationem tempora, accusamus harum minima voluptas in modi excepturi ratione aliquam recusandae distinctio cum a mollitia porro provident enim impedit non. Cum, hic modi. Praesentium necessitatibus consectetur saepe ducimus placeat veritatis perspiciatis modi dolores at eos aliquid quibusdam dignissimos iste autem magni omnis cupiditate repellat, laborum aut! Doloremque ipsum accusantium saepe doloribus a explicabo sapiente sint eaque maiores, unde nulla nihil quisquam harum quo dolorum perferendis vitae impedit sed. Ex ab voluptas, cumque voluptate sit aperiam debitis consequatur officia."
+            }
+          />
         </div>
       </div>
     </div>
