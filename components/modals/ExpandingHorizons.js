aef717fa6125b2c2b9bfc2bfdf78ef23f1663fc1
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-02-09 13:58:56 +0530

    animation

--- components/modals/ExpandingHorizons.js
+++ components/modals/ExpandingHorizons.js
@@ -1,5 +1,6 @@
 /* eslint-disable @next/next/no-img-element */
 import React from "react";
+import { Fade } from "react-reveal";
 
 function FuelingSuccess({
   expandingHorizonsModalOpen,
@@ -7,46 +8,48 @@ function FuelingSuccess({
 }) {
   return (
     <div>
-      {expandingHorizonsModalOpen && (
-        <div className="bg-[#010417] fixed inset-0 h-full w-full z-30 p-16 overflow-auto">
-          <div>
+      <Fade when={expandingHorizonsModalOpen} duration={500}>
+        {expandingHorizonsModalOpen && (
+          <div className="bg-[#010417] fixed inset-0 h-full w-full z-30 p-16 overflow-auto">
+            <div>
+              <img
+                onClick={() => setExpandingHorizonsModalOpen(false)}
+                src="/images/hamBurgerMenu.svg"
+                className="h-5 cursor-pointer"
+                alt=""
+              />
+            </div>
+            <div className="mt-16 w-[65%]">
+              <h1 className="uppercase text-5xl text-white font-extrabold">
+                Expanding Horizons
+              </h1>
+              <div className="mt-10 text-white space-y-8">
+                <div>
+                  <p className="mt-1 leading-7 font-poppins">
+                    Sharing value <br /> By marketing and developing of our
+                    &apos;Ultimate Destination&apos; project, the universe{" "}
+                    <br /> will be enlightened with our plans, all eyes will be
+                    on FROGGIES Token. <br />
+                    <br /> Valued on all planets <br /> We will continue
+                    collaborations with third parties to turn FROGGIES Token
+                    into a <br /> means of real-life payments. Envision online
+                    stores where you can check-out <br /> using your Froggies
+                    Token! <br />
+                    <br /> Full Booster Launch <br /> The future release of our
+                    entertainment platform will be entirely powered by <br />{" "}
+                    $FRGST. A whole FROGGIEVERSE of play to earn gaming systems.
+                  </p>
+                </div>
+              </div>
+            </div>
             <img
-              onClick={() => setExpandingHorizonsModalOpen(false)}
-              src="/images/hamBurgerMenu.svg"
-              className="h-5 cursor-pointer"
+              src="/images/expandingHorizons.svg"
+              className={`fixed bottom-0 right-1/2 translate-x-1/2  transition-all duration-700}`}
               alt=""
             />
           </div>
-          <div className="mt-16 w-[65%]">
-            <h1 className="uppercase text-5xl text-white font-extrabold">
-              Expanding Horizons
-            </h1>
-            <div className="mt-10 text-white space-y-8">
-              <div>
-                <p className="mt-1 leading-7 font-poppins">
-                  Sharing value <br /> By marketing and developing of our
-                  &apos;Ultimate Destination&apos; project, the universe <br />{" "}
-                  will be enlightened with our plans, all eyes will be on
-                  FROGGIES Token. <br />
-                  <br /> Valued on all planets <br /> We will continue
-                  collaborations with third parties to turn FROGGIES Token into
-                  a <br /> means of real-life payments. Envision online stores
-                  where you can check-out <br /> using your Froggies Token!{" "}
-                  <br />
-                  <br /> Full Booster Launch <br /> The future release of our
-                  entertainment platform will be entirely powered by <br />{" "}
-                  $FRGST. A whole FROGGIEVERSE of play to earn gaming systems.
-                </p>
-              </div>
-            </div>
-          </div>
-          <img
-            src="/images/stellarInnovation.svg"
-            className={`fixed bottom-0 right-0  transition-all duration-700}`}
-            alt=""
-          />
-        </div>
-      )}
+        )}
+      </Fade>
     </div>
   );
 }
--- components/modals/StellarInnovation.js
+++ components/modals/StellarInnovation.js
@@ -1,5 +1,6 @@
 /* eslint-disable @next/next/no-img-element */
 import React from "react";
+import { Fade } from "react-reveal";
 
 function FuelingSuccess({
   stellarInnovationModalOpen,
@@ -7,46 +8,48 @@ function FuelingSuccess({
 }) {
   return (
     <div>
-      {stellarInnovationModalOpen && (
-        <div className="bg-[#010417] fixed inset-0 h-full w-full z-30 p-16 overflow-auto">
-          <div>
+      <Fade when={stellarInnovationModalOpen} duration={500}>
+        {stellarInnovationModalOpen && (
+          <div className="bg-[#010417] fixed inset-0 h-full w-full z-30 p-16 overflow-auto">
+            <div>
+              <img
+                onClick={() => setStellarInnovationModalOpen(false)}
+                src="/images/hamBurgerMenu.svg"
+                className="h-5 cursor-pointer"
+                alt=""
+              />
+            </div>
+            <div className="mt-16 w-[65%]">
+              <h1 className="uppercase text-5xl text-white font-extrabold">
+                Stellar innovation
+              </h1>
+              <div className="mt-10 text-white space-y-8">
+                <div>
+                  <p className="mt-1 leading-7">
+                    Stellar amusement <br /> As we release our first part of the
+                    entertainment platform we will provide our <br /> holders
+                    with more usability for their Froggies Token <br />
+                    <br /> NFT with galactic use-case <br />
+                    <br /> Imagine an NFT that does not only have the value of
+                    art, but also contains an <br /> actual use-case. Think of
+                    rewards, staking NFTs, lotteries and more. <br />
+                    <br /> Ultimate destination <br /> Prepare for the Ultimate
+                    Destination by starting the development of something <br />{" "}
+                    unique (we cannot disclose, others are watching). This will
+                    put Froggies Token on <br /> the map for becoming one of the
+                    top Memecoins on the market.
+                  </p>
+                </div>
+              </div>
+            </div>
             <img
-              onClick={() => setStellarInnovationModalOpen(false)}
-              src="/images/hamBurgerMenu.svg"
-              className="h-5 cursor-pointer"
+              src="/images/stellarInnovation.svg"
+              className={`fixed bottom-0 right-0  transition-all duration-700}`}
               alt=""
             />
           </div>
-          <div className="mt-16 w-[65%]">
-            <h1 className="uppercase text-5xl text-white font-extrabold">
-              Stellar innovation
-            </h1>
-            <div className="mt-10 text-white space-y-8">
-              <div>
-                <p className="mt-1 leading-7">
-                  Stellar amusement <br /> As we release our first part of the
-                  entertainment platform we will provide our <br /> holders with
-                  more usability for their Froggies Token <br />
-                  <br /> NFT with galactic use-case <br />
-                  <br /> Imagine an NFT that does not only have the value of
-                  art, but also contains an <br /> actual use-case. Think of
-                  rewards, staking NFTs, lotteries and more. <br />
-                  <br /> Ultimate destination <br /> Prepare for the Ultimate
-                  Destination by starting the development of something <br />{" "}
-                  unique (we cannot disclose, others are watching). This will
-                  put Froggies Token on <br /> the map for becoming one of the
-                  top Memecoins on the market.
-                </p>
-              </div>
-            </div>
-          </div>
-          <img
-            src="/images/stellarInnovation.svg"
-            className={`fixed bottom-0 right-0  transition-all duration-700}`}
-            alt=""
-          />
-        </div>
-      )}
+        )}
+      </Fade>
     </div>
   );
 }
