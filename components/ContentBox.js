2051807d33d6dde7fe60e047bef0cccc4332f925
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-02-11 00:03:12 +0530

    scroll updated

--- components/ContentBox.js
+++ components/ContentBox.js
@@ -15,9 +15,12 @@ function ContentBox({ count, active, setActive, children }) {
   return (
     <div
       id={`${count}`}
-      className="text-white h-screen lg:p-7 px-5 py-10 w-full shrink-0 max-w-full lg:min-h-screen lg:h-fit overflow-hidden snap-start lg:snap-none"
+      className="text-white relative h-screen lg:p-7 px-5 py-10 w-full shrink-0 max-w-full lg:min-h-screen lg:h-fit overflow-hidden snap-start lg:snap-none"
     >
-      <div ref={ref} className="h-10"></div>
+      <div
+        ref={ref}
+        className="h-10 asbolute top-0 w-10 ml-auto lg:ml-0 lg:w-full lg:static"
+      ></div>
       <div className="sm:w-screen ">{children}</div>
     </div>
   );
