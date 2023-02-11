48d706bfb7790bc1aff7f091766fca04d13e689a
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-02-11 19:36:52 +0530

    navbar updated

--- components/ExpandedNav.js
+++ components/ExpandedNav.js
@@ -1,11 +1,12 @@
 /* eslint-disable @next/next/no-img-element */
 import Link from "next/link";
+import { Menu } from "@headlessui/react";
 import React from "react";
 
 function ExpandedNav({ setExpandedOpen }) {
   return (
-    <div className="fixed inset-0 bg-primaryBg h-full w-full z-40">
-      <div className="flex py-4 px-5 items-center">
+    <div className="fixed inset-0 bg-primaryBg h-full w-full z-40 overflow-auto py-5">
+      <div className="flex py-4 px-5 items-center fixed top-0 inset-x-0">
         <Link href={"/"}>
           <img src={"/images/logoPrimary.png"} className="w-20" alt="" />
         </Link>
@@ -38,15 +39,87 @@ function ExpandedNav({ setExpandedOpen }) {
             <span>Manifesto</span>
           </Link>
         </li>
-        <li>
-          <span>Buy</span>
+        <li className="text-center">
+          <Menu>
+            <Menu.Button>Buy</Menu.Button>
+            <Menu.Items>
+              <ul className="text-xs text-primaryGrey space-y-5 mt-5 bg-white/10 p-5">
+                <li>
+                  <Link
+                    href={
+                      "https://www.pancakeswap.finance/swap#/swap?outputCurrency=0x8e9f5175fD4Fb3515C85F4dAe85706A93498A93a"
+                    }
+                  >
+                    <span>PancakeSwap</span>
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href={
+                      "https://poocoin.app/swap?outputCurrency=0x7029994f28fd39ff934A96b25591D250A2183f7"
+                    }
+                  >
+                    Poocoin
+                  </Link>
+                </li>
+                <li>
+                  <Link href={`https://www.coinstore.com/#/spot/FRGSTUSDT`}>
+                    Coinstore
+                  </Link>
+                </li>
+              </ul>
+            </Menu.Items>
+          </Menu>
         </li>
+
         <li>
           <Link href={"/Interstellar"}></Link>
           <span>Stellar Doc</span>
         </li>
-        <li>
-          <span>More</span>
+        <li className="text-center">
+          <Menu>
+            <Menu.Button>More</Menu.Button>
+            <Menu.Items>
+              <ul className="text-xs text-primaryGrey space-y-5 mt-5 bg-white/10 p-5">
+                <li>
+                  <Link href={"/Partners"}>
+                    <span>Partners</span>
+                  </Link>
+                </li>
+                <li>
+                  <Link href={"/Sitemap"}>Sitemap</Link>
+                </li>
+                <li>
+                  <Link href={`/Astronomics`}>Astronomics</Link>
+                </li>
+                <li>
+                  <Link href={`/FAQ`}>FAQ</Link>
+                </li>
+                <li>
+                  <Link href={`https://nanuqe.com/froggies-token/`}>KYC</Link>
+                </li>
+                <li>
+                  <Link
+                    href={`https://bscscan.com/token/0x7029994f28fd39ff934a96b25591d250a2183f67`}
+                  >
+                    BSC Scan
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href={`https://www.coingecko.com/en/coins/froggies-token`}
+                  >
+                    Coin Gecko
+                  </Link>
+                </li>
+                <li>
+                  <Link href={`https://coinmarketcap.com/currencies/froggies/`}>
+                    CMC
+                  </Link>
+                </li>
+              </ul>
+            </Menu.Items>
+          </Menu>
         </li>
         <li>
           <Link href={"https://t.me/officialfroggiestoken"}>
