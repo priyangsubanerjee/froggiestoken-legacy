2d0cabe969e87a5e96612ea1eca0899c3d9b87f1
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-02-13 23:00:14 +0530

    partners updated

--- pages/Partners.js
+++ pages/Partners.js
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 /* eslint-disable react/jsx-key */
 import NavbarDark from "@/components/NavbarDark";
 import React from "react";
@@ -29,18 +30,89 @@ function Partners() {
         <div className="flex w-[40%] overflow-hidden relative">
           <div className="absolute top-0 inset-x-0 h-44 bg-gradient-to-b from-white via-white/90 to-transparent z-20"></div>
           <Marquee className="rotate-90" gradient={false} speed={20}>
-            <h1 className="-rotate-90">heloo</h1>
-            <h1 className="-rotate-90">heloo</h1>
-            <h1 className="-rotate-90">heloo</h1>
-            <h1 className="-rotate-90">heloo</h1>
-            <h1 className="-rotate-90">heloo</h1>
-            <h1 className="-rotate-90">heloo</h1>
-            <h1 className="-rotate-90">heloo</h1>
-            <h1 className="-rotate-90">heloo</h1>
-            <h1 className="-rotate-90">heloo</h1>
-            <h1 className="-rotate-90">heloo</h1>
-            <h1 className="-rotate-90">heloo</h1>
-            <h1 className="-rotate-90">heloo</h1>
+            <div className="-rotate-90">
+              <img
+                src="/partners/yahoo.png"
+                className="h-24 grayscale"
+                alt=""
+              />
+            </div>
+            <div className="-rotate-90">
+              <img
+                src="/partners/yahoo_m.png"
+                className="h-24 grayscale"
+                alt=""
+              />
+            </div>
+            <div className="-rotate-90">
+              <img
+                src="/partners/binance.png"
+                className="h-24 grayscale"
+                alt=""
+              />
+            </div>
+            <div className="-rotate-90">
+              <img
+                src="/partners/coinstore.png"
+                className="h-24 grayscale"
+                alt=""
+              />
+            </div>
+            <div className="-rotate-90">
+              <img
+                src="/partners/coingecko.png"
+                className="h-32 grayscale"
+                alt=""
+              />
+            </div>
+            <div className="-rotate-90">
+              <img
+                src="/partners/crypto.png"
+                className="h-24 grayscale"
+                alt=""
+              />
+            </div>
+            <div className="-rotate-90">
+              <img
+                src="/partners/hypemytoken.png"
+                className="h-24 grayscale"
+                alt=""
+              />
+            </div>
+            <div className="-rotate-90">
+              <img
+                src="/partners/twitter.png"
+                className="h-24 grayscale"
+                alt=""
+              />
+            </div>
+            <div className="-rotate-90">
+              <img src="/partners/c.png" className="h-24 grayscale" alt="" />
+            </div>
+            <div className="-rotate-90">
+              <img
+                src="/partners/coindesk.png"
+                className="h-24 grayscale"
+                alt=""
+              />
+            </div>
+            <div className="-rotate-90">
+              <img
+                src="/partners/nasdaq.png"
+                className="h-24 grayscale"
+                alt=""
+              />
+            </div>
+            <div className="-rotate-90">
+              <img
+                src="/partners/bloomberg.png"
+                className="h-24 grayscale"
+                alt=""
+              />
+            </div>
+            <div className="-rotate-90">
+              <img src="/partners/ibt.png" className="h-24 grayscale" alt="" />
+            </div>
           </Marquee>
           <Marquee
             direction="right"
@@ -48,18 +120,69 @@ function Partners() {
             gradient={false}
             speed={20}
           >
-            <h1 className="-rotate-90">heloo</h1>
-            <h1 className="-rotate-90">heloo</h1>
-            <h1 className="-rotate-90">heloo</h1>
-            <h1 className="-rotate-90">heloo</h1>
-            <h1 className="-rotate-90">heloo</h1>
-            <h1 className="-rotate-90">heloo</h1>
-            <h1 className="-rotate-90">heloo</h1>
-            <h1 className="-rotate-90">heloo</h1>
-            <h1 className="-rotate-90">heloo</h1>
-            <h1 className="-rotate-90">heloo</h1>
-            <h1 className="-rotate-90">heloo</h1>
-            <h1 className="-rotate-90">heloo</h1>
+            <div className="-rotate-90">
+              <img
+                src="/partners/marketwatch.png"
+                className="h-32 grayscale"
+                alt=""
+              />
+            </div>
+            <div className="-rotate-90">
+              <img
+                src="/partners/digitaljournal.png"
+                className="h-32 grayscale"
+                alt=""
+              />
+            </div>
+            <div className="-rotate-90">
+              <img
+                src="/partners/coinmarketcap.png"
+                className="h-32 grayscale"
+                alt=""
+              />
+            </div>
+            <div className="-rotate-90">
+              <img
+                src="/partners/forbes.png"
+                className="h-32 grayscale"
+                alt=""
+              />
+            </div>
+            <div className="-rotate-90">
+              <img
+                src="/partners/cointelegraph.png"
+                className="h-32 grayscale"
+                alt=""
+              />
+            </div>
+            <div className="-rotate-90">
+              <img
+                src="/partners/pancakeswap.png"
+                className="h-32 grayscale"
+                alt=""
+              />
+            </div>
+            <div className="-rotate-90">
+              <img
+                src="/partners/newsfile.png"
+                className="h-32 grayscale"
+                alt=""
+              />
+            </div>
+            <div className="-rotate-90">
+              <img
+                src="/partners/coinsbit.png"
+                className="h-32 grayscale"
+                alt=""
+              />
+            </div>
+            <div className="-rotate-90">
+              <img
+                src="/partners/coinlore.png"
+                className="h-32 grayscale"
+                alt=""
+              />
+            </div>
           </Marquee>
           <div className="absolute bottom-0 inset-x-0 h-44 bg-gradient-to-t from-white to-transparent z-20"></div>
         </div>
