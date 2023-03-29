b0b55a905eefc53daa80e0b2d7a2f4757a529b11
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-03-29 20:28:15 +0530

    scroll ref

--- components/ContentBox.js
+++ components/ContentBox.js
@@ -14,12 +14,12 @@ function ContentBox({ count, active, setActive, children }) {
   }, [isInView]);
   return (
     <div className="lg:min-h-screen lg:h-fit h-screen overflow-auto text-white relative lg:p-7 px-5 py-5 w-full shrink-0 max-w-full snap-mandatory snap-start lg:snap-none scrollbar-hide">
+      <div
+        ref={ref}
+        className="h-[1px] absolute top-[50%] bg-red-50 w-10 ml-auto lg:ml-0 lg:w-full"
+      ></div>
       <div id={`${count}`} className="">
-        <div
-          ref={ref}
-          className="h-10 asbolute top-0 w-10 ml-auto lg:ml-0 lg:w-full lg:static"
-        ></div>
-        <div id="scrollContent" className="sm:w-screen">
+        <div id="scrollContent" className="sm:w-screen mt-12 lg:mt-0">
           {children}
         </div>
       </div>
